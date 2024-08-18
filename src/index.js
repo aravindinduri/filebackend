3774ef8b575a9324dceea8a02f33aebb4495e35d
aravindinduri <thearavindinduri@gmail.com>
2024-08-18 23:02:32 +0530

    added changes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ app.use(cors({
     origin : process.env.CORS_ORIGIN,
     credentials :true,
     methods: ['POST', 'GET', 'DELETE', 'PUT'],
-    exposedHeaders: ["set-cookie"],
 }));
 
 app.use(express.json({limit :"20kb"}));
